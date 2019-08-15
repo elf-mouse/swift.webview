2a982e452f44e4a5c5cb722647d37a4786cdc9ed
Elf-mousE <ifmouz@gmail.com>
2019-08-15 10:58:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple Universal Webview App
+
+This is an Xcode 6 Universal project that allows you to wrap your website or web app in a super simple iOS app.
+
+# Setup
+- Clone this repo, or hit "Download ZIP" in the Github sidebar.
+
+### Local mode (recommended)
+- Replace the stuff in `www` with your own mobile website.
+- Hit Run!
+
+### Internet mode
+- In `ViewController.swift`, switch around the commented out URL lines.
+- Set your URL...
+- Hit Run!
+
+# Why?
+
+So you can build an iOS app in HTML/CSS/JS without using Cordova. 
+
+HTML web apps are totally awesome for layout content and typography. Tons of big apps like Instagram use HTML layouts and CSS styling for parts of their apps.
+
+If you are a web developer who wants to release an iOS app, this should help you cut some corners when it comes to learning Swift.
+
+# Ideal use
+
+Single page web app with on-screen/no navigation. For example, a game, interactive page, web based slideshow, etc.
---
+++ Simple Universal Webview App/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Simple Universal Webview App
+//
+//  Created by Nabil Freeman on 19/06/2015.
+//  Copyright (c) 2015 Freeman Industries. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    var wkWebView: WKWebView?
+    var uiWebView: UIWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        // Get main screen rect size
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        
+        // Construct frame where webview will be pop
+        let frameRect: CGRect = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+
+        // Create url request from local index.html file located in web_content
+        let url: NSURL = NSBundle.mainBundle().URLForResource("web_content/index", withExtension: "html")!
+        
+        // Create a url request that points to a remote server (uncomment this line to use a remote url)
+        // let url: NSURL = NSURL(string: "http://example.com")!;
+
+        let requestObj: NSURLRequest = NSURLRequest(URL: url);
+        
+        // Test operating system
+        if NSProcessInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 8, minorVersion: 0, patchVersion: 0)) {
+            
+            self.wkWebView = WKWebView(frame: frameRect)
+            self.wkWebView?.loadRequest(requestObj)
+            self.view.addSubview(self.wkWebView!)
+
+        } else {
+
+            self.uiWebView = UIWebView(frame: frameRect)
+            self.uiWebView?.loadRequest(requestObj)
+            self.view.addSubview(self.uiWebView!)
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Commented:    black status bar.
+    //Uncommented:  white status bar.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+}
+
